1518e25ad4674e549cebd5b9ddb9889004a9aed0
MarkXtenda <mark.i2p.1998@gmail.com>
2022-07-29 17:25:53 +1000

    done

---
+++ makefile
@@ -0,0 +1,2 @@
+git:
+	git add -A ; git commit -m "done" ; git push
--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  squared_array = Array.new()
+    array.each do |element|
+    squared_array << element*element
+  end
+  squared_array
 end
+
+print square_array([2,3])
